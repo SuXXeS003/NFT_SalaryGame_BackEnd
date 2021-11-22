2f15a324bac3545c16d7b8bfdfbfb61263d97aaa
SuXXeS <63883201+SuXXeS003@users.noreply.github.com>
2021-11-22 22:16:01 +0100

    sec1 - contract: 0xEd25bB745126617c166D4c05a398eAEe8b4f1Eb3

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,9 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyEpicGame {
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "hardhat/console.sol";
+import "./libraries/Base64.sol";
+
+contract MyEpicGame is ERC721 {
 
     struct CharacterAttributes {
         uint characterIndex;
@@ -12,18 +20,29 @@ contract MyEpicGame {
         string imageURI;
         uint arguments;
         uint maxArguments;
-        uint attackDamage;
+        uint persuasion;
     }
 
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     CharacterAttributes[] defaultCharacters;
+
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
 
 
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint[] memory characterArguments,
-        uint[] memory characterAttackDmg
+        uint[] memory characterPersuasion
     ) 
+        ERC721("Worker","WORKER")
     {
         for(uint i = 0; i < characterNames.length;i += 1) {
             defaultCharacters.push(CharacterAttributes({
@@ -32,12 +51,62 @@ contract MyEpicGame {
                 imageURI: characterImageURIs[i],
                 arguments: characterArguments[i],
                 maxArguments: characterArguments[i],
-                attackDamage: characterAttackDmg[i]
+                persuasion: characterPersuasion[i]
             }));
 
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s with %s Arguments, img %s", c.name, c.arguments, c.imageURI);
         }
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            arguments: defaultCharacters[_characterIndex].arguments,
+            maxArguments: defaultCharacters[_characterIndex].maxArguments,
+            persuasion: defaultCharacters[_characterIndex].persuasion
+        });
+
+        console.log("Minted NFT with tokenId %s and characterIndex %s", newItemId, _characterIndex);
+        nftHolders[msg.sender] = newItemId;
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+        string memory strArguments = Strings.toString(charAttributes.arguments);
+        string memory strMaxArguments = Strings.toString(charAttributes.maxArguments);
+        string memory strPersuasion = Strings.toString(charAttributes.persuasion);
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        charAttributes.name,
+                        ' -- NFT #: ',
+                        Strings.toString(_tokenId),
+                        '", "description": "This is an NFT that lets people play in the game Get More Money", "image": "',
+                        charAttributes.imageURI,
+                        '", "attributes": [ { "trait_type": "Arguments", "value": ',strArguments,', "max_arguments":',strMaxArguments,'}, { "trait_type": "Persuasion", "value": ',
+                        strPersuasion,'}]}'
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return output;
     }
 }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,16 +8,29 @@ const main = async () => {
         [100, 200, 300],
         [100, 50, 25]
     );
+
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 
     let txn;
 
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
+    console.log("Minted NFT #3");
 
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("Token URI:", returnedTokenUri);
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #4");
+
+    console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
